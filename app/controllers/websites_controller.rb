2824ec13a48a68eef941e45e9b96425785cb7727
fanna <fanna993@gmail.com>
2015-07-14 13:32:26 +0200

    Adding Nokogiri

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -2,10 +2,6 @@ class WebsitesController < ApplicationController
 	  require 'open-uri'
 	  require 'nokogiri'
 
-
-
-	  
-
   def new
   	@website = Website.new
   	@websites = Website.all
@@ -17,21 +13,21 @@ class WebsitesController < ApplicationController
   	if @website.save
   		redirect_to new_website_path
   	end
-  end
 
-  def website_params
-    params.require(:website).permit(:url)
-  end
-
-  def grab
-  	input_url = params[:url]
+  	input_url = website_params
   	url = input_url.to_s
+  	url.slice! '{"url"=>"'
+  	url.slice! '"}'
   	data = Nokogiri::HTML(open(url))
   	contents = data.css("html")
   	contents.each do |content|
 		content.at_css("h1").text.strip
 		content.at_css("p").text.strip
-  	end
+	end
+  end
+
+  def website_params
+    params.require(:website).permit(:url)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  post "/grab", to: "websites#grab" 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
