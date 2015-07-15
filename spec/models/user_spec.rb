2cb58ef2c1f2a42299451876c0c7c898c2f3e9a5
fanna <fanna993@gmail.com>
2015-07-16 00:23:55 +0200

    Adding initial RSpec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+describe User do
+
+  it { should have_db_column(:username).of_type(:string) }
+  it { should have_db_column(:email).of_type(:string) }
+  it { should have_db_column(:encrypted_password).of_type(:string) }
+
+  it { should validate_uniqueness_of(:username) }
+  it { should validate_length_of(:username).is_at_least(4) }
+  it { should validate_length_of(:username).is_at_most(16) }
+
+  it { should validate_uniqueness_of(:email) }
+end
