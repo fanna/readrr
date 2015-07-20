4a5019faede9d55e5bf325c879549029283298f1
fanna <fanna993@gmail.com>
2015-07-20 12:15:05 +0200

    Fixing the view

--- app/assets/javascripts/websites.js
+++ app/assets/javascripts/websites.js
@@ -1,51 +1,37 @@
 $(document).ready(function() {
-	$("#read").click(function () {
-		//var myString =  $('#final_array').text();
-		//var arr = $.map($('#final_array').text(), function(el) { return el; })
-		//var arrayLength = myString.length;
-		//console.log(myString);
-		//for (var i = 0; i < arrayLength; i++) {
-    	//	console.log(myStringArray[i]);
+    $("#execute").click(function () {
 
-		//}
-		//$.post("http://localhost:4567/", { myString: myString });
+	var myString =  $('#final_array').text();
+	var res = myString.split(" ");
+	var arrayLength = res.length;
+	for (var i = 0; i < arrayLength; i++) {
+	    var myFinalString = res[i];
+	    console.log(myFinalString);
+	    $.post("http://localhost:4567/", { myFinalString: myFinalString });
 
+	    if ($('[name="speed1"]').is(':checked')){ var mySpeed = "200"; };
+	    if ($('[name="speed2"]').is(':checked')){ var mySpeed = "250"; };
+	    if ($('[name="speed3"]').is(':checked')){ var mySpeed = "300"; };
 
-		if ($('[name="speed1"]').is(':checked')){ var mySpeed = "200"; };
-		if ($('[name="speed2"]').is(':checked')){ var mySpeed = "250"; };
-		if ($('[name="speed3"]').is(':checked')){ var mySpeed = "300"; };
+	   $.post("http://localhost:4567/", { mySpeed: mySpeed });
+	    //TODO: fix color statements
+	    if ($('#toggle-one').bootstrapToggle('on')) {
+		var myColor = "classic";
+	    };
 
-		$.post("http://localhost:4567/", { mySpeed: mySpeed });
-		//TODO: fix color statements
-	  if ($('#toggle-one').bootstrapToggle('on')) {
-	  		var myColor = "classic";
-	  };
+	    if ($('#toggle-two').bootstrapToggle('on')) {
+		var myColor = "gay_clr";
+	    };
 
-	  if ($('#toggle-two').bootstrapToggle('on')) {
-	  		var myColor = "gay_clr";
-	  };
+	    if ($('#toggle-three').bootstrapToggle('on')) {
+		var myColor = "repub_clr";
+	    };
 
-	  if ($('#toggle-three').bootstrapToggle('on')) {
-	  		var myColor = "repub_clr";
-	  };
+	    $.post("http://localhost:4567/", { myColor: myColor });
+	
 
-		$.post("http://localhost:4567/", { myColor: myColor });
-
-	});
-
-	$("#execute").click(function () {
-
-		var myString =  $('#final_array').text();
-		var res = myString.split(" ");
-		var arrayLength = res.length;
-		for (var i = 0; i < arrayLength; i++) {
-    		var myFinalString = res[i];
-    		//console.log(myFinalString);
-    		$.post("https://scraprr.herokuapp.com/", { myFinalString: myFinalString });
-
-
-		}
-	});
+	}
+    });
 
 });
 
